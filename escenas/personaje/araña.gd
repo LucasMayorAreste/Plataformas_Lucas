ea953c066d0f24d8f062cebdd3b3fc7f69b8501c
LucasMayorAreste <lucas.mayor@metrikamind.com>
2026-06-10 19:19:14 +0200

    Nuevo nivel

--- escenas/personaje/araña.gd
+++ escenas/personaje/araña.gd
@@ -3,24 +3,47 @@ extends Area2D
 @export var distancia_movimiento: float = 150.0
 @export var tiempo_por_trayecto: float = 1.5
 
+# --- NUEVOS INTERRUPTORES PARA EL INSPECTOR ---
+@export var movimiento_vertical: bool = false
+@export var hace_saltitos: bool = false
+
 func _ready() -> void:
-	# Enganchamos la alarma a TODO: cuerpos físicos y otras áreas (como la de tu personaje)
+	# El código que ya te funciona para matar (cuerpos y áreas)
 	body_entered.connect(_intentar_matar_cuerpo)
 	area_entered.connect(_intentar_matar_area)
 	_patrullar()
 
+func _patrullar():
+	if movimiento_vertical:
+		# 1. ARAÑA DE PARED (Sube y baja)
+		var tween = create_tween().set_loops()
+		tween.tween_property(self, "position:y", position.y - distancia_movimiento, tiempo_por_trayecto)
+		tween.tween_property(self, "position:y", position.y, tiempo_por_trayecto)
+		
+	else:
+		# 2. ARAÑA DE SUELO
+		if hace_saltitos:
+			# Va de izquierda a derecha pero botando
+			var tween_x = create_tween().set_loops()
+			tween_x.tween_property(self, "position:x", position.x + distancia_movimiento, tiempo_por_trayecto)
+			tween_x.tween_property(self, "position:x", position.x, tiempo_por_trayecto)
+			
+			var tween_y = create_tween().set_loops()
+			tween_y.tween_property(self, "position:y", position.y - 30, 0.3).set_trans(Tween.TRANS_SINE)
+			tween_y.tween_property(self, "position:y", position.y, 0.3).set_trans(Tween.TRANS_SINE)
+		else:
+			# Va de izquierda a derecha normal (plana)
+			var tween = create_tween().set_loops()
+			tween.tween_property(self, "position:x", position.x + distancia_movimiento, tiempo_por_trayecto)
+			tween.tween_property(self, "position:x", position.x, tiempo_por_trayecto)
+
+
+# --- FUNCIONES DE MUERTE (INTACTAS) ---
 func _intentar_matar_cuerpo(body):
-	# Si la araña toca el cuerpo principal del personaje
 	if body.has_method("_on_area_2d_body_entered"):
 		body._on_area_2d_body_entered(self)
 
 func _intentar_matar_area(area):
-	# Si la araña toca la "caja de daño" (Area2D) del personaje
-	var padre = area.get_parent() # Subimos un nivel para encontrar al personaje
+	var padre = area.get_parent() 
 	if padre != null and padre.has_method("_on_area_2d_body_entered"):
 		padre._on_area_2d_body_entered(self)
-
-func _patrullar():
-	var tween = create_tween().set_loops()
-	tween.tween_property(self, "position:y", position.y - distancia_movimiento, tiempo_por_trayecto)
-	tween.tween_property(self, "position:y", position.y, tiempo_por_trayecto)
